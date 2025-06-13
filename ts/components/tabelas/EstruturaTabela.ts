c57bb9bb82f60fd66873953ce7183b8b80cc971b
Chellp <karlakirchhoff.contato@gmail.com>
2025-06-13 15:03:53 -0300

    implementação da funcionalidade para gerar linhas na tabela, todas as funcionalidades da classe 'EstruturaTabela.ts' estão funcionando

--- ts/components/tabelas/EstruturaTabela.ts
+++ ts/components/tabelas/EstruturaTabela.ts
@@ -1,3 +1,6 @@
+interface Registro {
+    [key: string]: any;  // Usando chave do tipo string e valor de qualquer tipo
+}
 
 export default class EstruturaTabela {
     tableHead: string = '';
@@ -5,6 +8,8 @@ export default class EstruturaTabela {
     titulo: string = '';
     tipoTabela: string = '';
     nomeTabela: string = '';
+    qtdColunas: number = 0;
+
 
     constructor(tableHead: Array<string>, tipoTabela: string, nomeTabela: string, titulo: string) {
 
@@ -15,46 +20,76 @@ export default class EstruturaTabela {
 
         //gerar células do cabeçalho
         let td: string = '';
-        for (let td of tableHead) {
-            td += `<td>${td}</td>`
+        for (let coluna of tableHead) {
+            td += `<td>${coluna}</td>`;
+            this.qtdColunas++;
         }
 
         //gerar cabeçalho
-        this.tableHead = `<thead><tr>${td}</tr></thead>`
+        this.tableHead = `<thead><tr>${td}</tr></thead>`;
     }
 
+    mostrarHeader() {
+        return this.tableHead;
+    }
+
+
+    //definir como privado
+    //recebe variável com conteúdo HTML de cada tipo de filtro e retorna o HTML deles juntos
     gerarFiltros(filtros: string[] = []) {
 
         let filtrosGerados: string = '';
 
-        for(let i of filtros){
+        for (let i of filtros) {
             filtrosGerados += i;
         }
 
         return filtrosGerados;
     }
 
-    criarTabela(filtros: string[] = []) {
+    //deifinir como privado
+    //funçao que cria html das linhas da tabela, recebe como parâmetro a quantidade de linhas para impressão em tela
+    gerarLinhasTabela(registros: Registro[]) {
+
+        //let totalRegistrosPermitidos: number = 10; ---- ATUALIZAÇÃO
+        let tableBody: string = '';
+        let tr: string = '';
+
+        //inicia a criação das linhas da tabela
+        //percorre o array de objetos
+        for (let objeto of registros) {
+
+            let td: string = '';
+
+            //percorre os valores do objeto
+            for (let chave in objeto) {
+
+
+                //verifica se a propiedade não é herdada
+                if (objeto.hasOwnProperty(chave)) {
+                    td += `<td>${objeto[chave]}</td>`;
+                }
+            }
+
+            tr += `<tr>${td}</tr>
+            `;
+        }
+
+        tableBody = tr
+
+        return tableBody;
+    }
+
+    criarTabela(filtros: string[] = [], registros: Registro[]) {
         let tabela: string;
 
         tabela = `
             <div class="titulo-filtros">
                 <h1>${this.titulo}</h1>
                 <div class="area-filtros">${this.gerarFiltros(filtros)}</div><!-- .area-filtros -->
-            </div> <!-- .titulo-filtros -->
-
+            </div>
             <table class="tabela-padrao" id="tabela-todos-relatorios">
-                <thead>
-                    <tr>
-                        <td>Data</td>
-                        <td>Hora</td>
-                        <td>Relatorio</td>
-                        <td> </td>
-                    </tr>
-                </thead>
-
-                <tbody></tbody>
-
+                ${this.tableHead} <tbody>${this.gerarLinhasTabela(registros)}</tbody>
             </table>
         `
 
@@ -62,3 +97,36 @@ export default class EstruturaTabela {
     }
 
 }
+
+
+
+
+//------------------------------ TESTES ----------------------------------------
+
+
+const teste = new EstruturaTabela(['tt', 'tt', 'hhh', '55'], 'tt', 'hh', 'mkkk');
+
+
+const testeobjeto: Record<string, any>[] = [{
+    "valor1": "valor 1",
+    "valor2": "valor 2",
+    "valor3": "valor 3",
+    "valor4": "valor 4"
+},
+{
+    "valor1": "valor 11",
+    "valor2": "valor 12",
+    "valor3": "valor 13",
+    "valor4": "valor 14"
+},
+{
+    "valor1": "valor 1s1",
+    "valor2": "valor 1s2",
+    "valor3": "valor 1s3",
+    "valor4": "valor 1s4"
+}
+
+];
+
+//console.log(teste.gerarLinhasTabela(testeobjeto));
+console.log(teste.criarTabela(['filtro 1', 'filtro 2'],testeobjeto));
