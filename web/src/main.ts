466b701e0c5ca0bb5e6b030333b1083de6f44a0d
Chellp <karlakirchhoff.contato@gmail.com>
2025-06-25 15:02:21 -0300

    implementação do vite

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,9 +3,9 @@ const data = new getDataHora();
 
 console.log(data.dataHoraPadrao()); */
 
-import { examesDB } from "./models/interfaces/InterfaceExames";
+//import { examesDB } from "./models/interfaces/InterfaceExames";
 
-console.log(examesDB);
+//console.log(examesDB);
 
 
 console.log('teste');
