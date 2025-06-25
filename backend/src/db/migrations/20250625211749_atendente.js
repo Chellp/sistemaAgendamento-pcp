92c8a0d1aaa70e0b84e14caa43bbe43c0d1d496a
Chellp <karlakirchhoff.contato@gmail.com>
2025-06-25 19:00:21 -0300

    implementando 'migrations' do knex

---
+++ backend/src/db/migrations/20250625211749_atendente.js
@@ -0,0 +1,20 @@
+const { table } = require("console");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('atendente', (table) => {
+        table.increments('cod').primary()
+        table.integer('id_perfil').notNullable(); //fk perfil
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('atendente');
+};
---
+++ backend/src/db/migrations/20250625214530_diretor.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('diretor', (table) => {
+        table.increments('cod').primary()
+        table.integer('id_perfil').notNullable(); //fk perfil
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('diretor');
+};
