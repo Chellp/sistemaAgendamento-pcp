b15365967a22f2da24d8ab14c01ae46339f2391a
Chellp <karlakirchhoff.contato@gmail.com>
2025-07-17 13:31:32 -0300

    remoção de arquivos frontend para substituição

--- web/src/components/padrao/componentsPadraoFooter.ts
+++
@@ -1,8 +0,0 @@
-export default function componentsPadraoFooter(){
-    const footer: string = `
-        <footer>
-            <img src="../img/logo-black.png" alt="">
-        </footer>
-    `;
-    return footer;
-}
--- web/src/components/padrao/componentsPadraoSecMainPrincipal.ts
+++
@@ -1,7 +0,0 @@
-import componentsPadraoFooter from "./componentsPadraoFooter"; const footer = componentsPadraoFooter();
-
-export default function componentsPadraoSecMainPrincipal(el: string){
-    const principal = `<section class="sec-main-principal" id="html-sec-main-principal">${el} ${footer}</section>`
-
-    return principal
-}
