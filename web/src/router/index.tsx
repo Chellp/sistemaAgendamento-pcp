40ec95cbcf8d42f34b3bd50cd69e0858f48d4e99
Chellp <karlakirchhoff.contato@gmail.com>
2025-07-23 13:15:51 -0300

    configuração inicial de rota frontend

---
+++ web/src/router/index.tsx
@@ -0,0 +1,17 @@
+//import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AgendamentoPrincipalPage from '../pages/Home'
+import CriarAgendamentoPage from '../pages/criarAgendamentoPage'
+import NotFound from '../pages/NotFound'
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<AgendamentoPrincipalPage />} />
+            <Route path='/novo-exame' element={<CriarAgendamentoPage />} />
+            <Route path='/*' element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
