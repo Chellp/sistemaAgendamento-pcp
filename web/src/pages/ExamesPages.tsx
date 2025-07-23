7387b2872cab1864511c2d370ffb134bc3fee9fa
Karla Kirchhoff <keikheyk@gmail.com>
2025-07-22 21:05:31 -0300

    pagina exames

---
+++ web/src/pages/ExamesPages.tsx
@@ -0,0 +1,13 @@
+import { Toolbar, Typography } from "@mui/material";
+
+
+export default function ExamesPages() {
+    return (
+        <>
+            <Toolbar></Toolbar>
+            <Typography variant='h1'>
+                Todos os Exames
+            </Typography>
+        </>
+    )
+}
